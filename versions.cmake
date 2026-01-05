331a6d85677f2f5310718706a6d038f696e2b2ec
Cold-Mint <cold_mint@qq.com>
2026-01-05 12:04:31 +0800

    Update the dependent version.

---
+++ versions.cmake
@@ -0,0 +1,20 @@
+#Variable definitions are generated through GlimmerWorksBuilder. Please do not edit them.
+#The dependencies of the repository can be defined through comments.
+#ocornut/imgui@latest
+#nlohmann/json@latest
+#libsdl-org/SDL@latest
+#libsdl-org/SDL_image@latest
+#libsdl-org/SDL_ttf@latest
+#mobius3/tweeny@latest
+#fmtlib/fmt@latest
+#Auburn/FastNoiseLite@latest
+#erincatto/box2d@latest
+set(OCORNUT_IMGUI_VERSION "v1.92.5")
+set(NLOHMANN_JSON_VERSION "v3.12.0")
+set(LIBSDL-ORG_SDL_VERSION "release-3.4.0")
+set(LIBSDL-ORG_SDL_IMAGE_VERSION "release-3.2.6")
+set(LIBSDL-ORG_SDL_TTF_VERSION "release-3.2.2")
+set(MOBIUS3_TWEENY_VERSION "v3.2.1")
+set(FMTLIB_FMT_VERSION "12.1.0")
+set(AUBURN_FASTNOISELITE_VERSION "v1.1.1")
+set(ERINCATTO_BOX2D_VERSION "v3.1.1")

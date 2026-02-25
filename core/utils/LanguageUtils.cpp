d424301b05db68903cac3046d3e58d3e01667491
Cold-Mint <cold_mint@qq.com>
2026-02-25 14:36:14 +0800

    Supports running on Windows.

--- core/utils/LanguageUtils.cpp
+++ core/utils/LanguageUtils.cpp
@@ -8,6 +8,10 @@
 #include <android/asset_manager_jni.h>
 #include <SDL3/SDL_system.h>
 #endif
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#include <locale>
+#endif
 
 std::string glimmer::LanguageUtils::getLanguage() {
 #ifdef __ANDROID__
@@ -31,6 +35,35 @@ std::string glimmer::LanguageUtils::getLanguage() {
     env->ReleaseStringUTFChars(languageJ, languageC);
     env->ReleaseStringUTFChars(countryJ, countryC);
     return result;
+#elif defined(_WIN32) || defined(_WIN64)
+    LCID localeId = GetUserDefaultUILanguage();
+    if (localeId == 0) {
+        localeId = GetSystemDefaultLCID();
+    }
+
+    WCHAR langCode[3] = {0};
+    WCHAR countryCode[3] = {0};
+    
+    if (!GetLocaleInfoW(localeId, LOCALE_SISO639LANGNAME, langCode, _countof(langCode))) {
+        wcscpy_s(langCode, L"en");
+    }
+    if (!GetLocaleInfoW(localeId, LOCALE_SISO3166CTRYNAME, countryCode, _countof(countryCode))) {
+        wcscpy_s(countryCode, L"US");
+    }
+
+    int langLen = WideCharToMultiByte(CP_UTF8, 0, langCode, -1, nullptr, 0, nullptr, nullptr);
+    int countryLen = WideCharToMultiByte(CP_UTF8, 0, countryCode, -1, nullptr, 0, nullptr, nullptr);
+    
+    std::string langStr(langLen, 0);
+    std::string countryStr(countryLen, 0);
+    
+    WideCharToMultiByte(CP_UTF8, 0, langCode, -1, &langStr[0], langLen, nullptr, nullptr);
+    WideCharToMultiByte(CP_UTF8, 0, countryCode, -1, &countryStr[0], countryLen, nullptr, nullptr);
+
+    langStr = langStr.substr(0, langStr.find_first_of('\0'));
+    countryStr = countryStr.substr(0, countryStr.find_first_of('\0'));
+
+    return langStr + "_" + countryStr;
 #else
     //linux
     if (const char *langEnv = getenv("LANG"); langEnv != nullptr) {

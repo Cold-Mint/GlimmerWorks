096ac0a272398ecae585aad47070194dcc52872c
Cold-Mint <68356914+Cold-Mint@users.noreply.github.com>
2026-06-15 11:38:46 +0800

    Refactor UUID formatting for better readability

--- core/utils/UUIDUtils.cpp
+++ core/utils/UUIDUtils.cpp
@@ -46,12 +46,12 @@ std::string glimmer::UUIDUtils::Generate()
     std::stringstream ss;
     ss << std::hex << std::setfill('0') << std::nouppercase;
 
-    ss << std::setw(8) << data[0]
-        << "-" << std::setw(4) << (data[1] >> 16)
-        << "-" << std::setw(4) << (data[1] & 0xFFFF)
-        << "-" << std::setw(4) << (data[2] >> 16)
-        << "-" << std::setw(4) << (data[2] & 0xFFFF)
-        << std::setw(8) << data[3];
+ss << std::setw(8) << data[0]
+    << "-" << std::setw(4) << (data[1] >> 16)
+    << "-" << std::setw(4) << (data[1] & 0xFFFF)
+    << "-" << std::setw(4) << (data[2] >> 16)
+    << "-" << std::setw(4) << (data[2] & 0xFFFF)
+    << "-" << std::setw(8) << data[3];
 
     return ss.str();
 }

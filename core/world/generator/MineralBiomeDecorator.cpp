8871097f924a40775597d6c0ddf2c1a932fc6269
Cold-Mint <cold_mint@qq.com>
2026-07-03 12:05:47 +0800

    Revise the grammar.

--- core/world/generator/MineralBiomeDecorator.cpp
+++ core/world/generator/MineralBiomeDecorator.cpp
@@ -31,64 +31,66 @@
 #include "ChunkGenerator.h"
 
 
-void glimmer::MineralBiomeDecorator::DecorationImp(WorldContext *worldContext, TerrainResult *terrainResult,
-                                                   MineralBiomeDecoratorResource *decoratorResource,
-                                                   BiomeResource *biomeResource,
+void glimmer::MineralBiomeDecorator::DecorationImp(WorldContext* worldContext, TerrainResult* terrainResult,
+                                                   MineralBiomeDecoratorResource* decoratorResource,
+                                                   BiomeResource* biomeResource,
                                                    std::unordered_map<TileLayerType, std::array<ResourceRef,
-                                                       CHUNK_AREA> > *tilesRefMap) {
-    const FastNoiseLite *noiseLite = decoratorResource->GetFastNoiseLite(worldSeed_);
-    if (noiseLite == nullptr) {
+                                                                          CHUNK_AREA>>* tilesRefMap)
+{
+    const FastNoiseLite* noiseLite = decoratorResource->GetFastNoiseLite(worldSeed_);
+    if (noiseLite == nullptr)
+    {
         return;
     }
     const float heightRange = decoratorResource->maxSpawnElevation - decoratorResource->minSpawnElevation;
-    if (heightRange <= 0.0001F) {
+    if (heightRange <= 0.0001F)
+    {
         return;
     }
-    std::array<ResourceRef, CHUNK_AREA> &targetLayer = tilesRefMap->at(
+    std::array<ResourceRef, CHUNK_AREA>& targetLayer = tilesRefMap->at(
         static_cast<TileLayerType>(decoratorResource->layerType));
-    for (int localX = 0; localX < CHUNK_SIZE; localX++) {
-        for (int localY = 0; localY < CHUNK_SIZE; localY++) {
+    for (int localX = 0; localX < CHUNK_SIZE; localX++)
+    {
+        for (int localY = 0; localY < CHUNK_SIZE; localY++)
+        {
             const int idx = localY * CHUNK_SIZE + localX;
             const TileVector2D absolutePosition = terrainResult->GetPosition() + TileVector2D(localX, localY);
             float elevation = ChunkGenerator::GetElevation(absolutePosition.y);
-            if (elevation > decoratorResource->maxSpawnElevation || elevation < decoratorResource->minSpawnElevation) {
+            if (elevation > decoratorResource->maxSpawnElevation || elevation < decoratorResource->minSpawnElevation)
+            {
                 continue;
             }
-            const TerrainTileResult &self = terrainResult->QueryTerrain(localX, localY);
-            if (self.terrainType != SOLID || self.biomeResource != biomeResource) {
+            const TerrainTileResult& self = terrainResult->QueryTerrain(localX, localY);
+            if (self.terrainType != SOLID || self.biomeResource != biomeResource)
+            {
                 continue;
             }
             const float normalizedOreNoise = (noiseLite->GetNoise(
-                                                  static_cast<float>(absolutePosition.x),
-                                                  static_cast<float>(absolutePosition.y)
-                                              ) + 1.0F) * 0.5F;
-            float currentSpawnThreshold;
+                static_cast<float>(absolutePosition.x),
+                static_cast<float>(absolutePosition.y)
+            ) + 1.0F) * 0.5F;
             const float elevationRatio = (elevation - decoratorResource->minSpawnElevation) / heightRange;
-            if (decoratorResource->invertOreSpawnByDepth) {
-                // Activate inversion: The deeper you go, the more ore there is.
-                // 开启反转：越向下，矿石越多
-                currentSpawnThreshold = std::lerp(
-                    decoratorResource->oreSpawnMinNoiseThreshold,
-                    decoratorResource->oreSpawnMaxNoiseThreshold,
-                    elevationRatio
-                );
-            } else {
-                // Closing inversion: The deeper you go, the less ore there is.
-                // 关闭反转：越向下，矿石越少
-                currentSpawnThreshold = std::lerp(
-                    decoratorResource->oreSpawnMaxNoiseThreshold,
-                    decoratorResource->oreSpawnMinNoiseThreshold,
-                    elevationRatio
-                );
-            }
+            float currentSpawnThreshold = decoratorResource->invertOreSpawnByDepth
+                                              ? std::lerp(
+                                                  decoratorResource->oreSpawnMinNoiseThreshold,
+                                                  decoratorResource->oreSpawnMaxNoiseThreshold,
+                                                  elevationRatio
+                                              )
+                                              : std::lerp(
+                                                  decoratorResource->oreSpawnMaxNoiseThreshold,
+                                                  decoratorResource->oreSpawnMinNoiseThreshold,
+                                                  elevationRatio
+                                              );
 
-            if (normalizedOreNoise > currentSpawnThreshold) {
+            if (normalizedOreNoise > currentSpawnThreshold)
+            {
                 targetLayer[idx] = decoratorResource->ore;
             }
         }
     }
 }
 
-glimmer::BiomeDecoratorType glimmer::MineralBiomeDecorator::GetBiomeDecoratorType() {
+glimmer::BiomeDecoratorType glimmer::MineralBiomeDecorator::GetBiomeDecoratorType()
+{
     return BiomeDecoratorType::MINERAL;
 }
